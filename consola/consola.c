4da95651251767aad893ce6fda07c1e6845580ed
rogerivan <ivan_lugano_18@hotmail.com>
2017-04-09 21:37:52 -0300

    Se crearon en consola.c las funciones crearSocket, conectarSocket y cerrarSocket, dentro del main se configuro las llamadas a estas funciones, queda por resolver porqué no se puede conectar el Socket al Servidor, queda pendiente a resolver

--- consola/consola.c
+++ consola/consola.c
@@ -1,9 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <arpa/inet.h>
-#include <sys/socket.h>
-#include <string.h>
-#include <unistd.h>
+
 #include "consola.h"
 
 
@@ -16,9 +11,39 @@ int cargarConfiguracion(){
      return 1;
 }
 
+int crearSocket(){
+	return socket(AF_INET,SOCK_STREAM,0);
+}
+
+int conectarSocket(int socket, struct sockaddr_in* direccionServidor){
+	//return connect(socket, (struct sockaddr*) &*direccionServidor, sizeof(struct sockaddr));
+	return connect(socket, &direccionServidor, sizeof(direccionServidor));
+}
+
+void cerrarSocket(int socket){
+	close(socket);
+}
+
 int main (void){
-	if(cargarConfiguracion() == 1){
+
+	if(cargarConfiguracion() == 1){  //para mas adelante se eliminará el if, solo se puso para verificar que estaba todo OK
 		printf("esta Ok");
-		return 0;
 	}
+
+    int socket = crearSocket();
+	if(socket == -1){  // Se valida de que se pudo crear el socket sin inconvenientes, retornando de "crearSocket un valor >=0.En caso de devolver -1 se visualizará el error.
+		perror("No se pudo crear el socket correctamente");
+	}
+
+	struct sockaddr_in direccionServidor; //Creo y configuro el servidor
+	direccionServidor.sin_family = AF_INET;
+	direccionServidor.sin_addr.s_addr = INADDR_ANY;
+	direccionServidor.sin_port = htons(8080);
+
+	if(conectarSocket(socket, &direccionServidor) != 0){ // no se está conectando al servidor
+		perror("No se realizó la conexión");
+	}
+
+
+	return 0;
 }
