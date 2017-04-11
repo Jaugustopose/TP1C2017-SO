63e95baebbcba21607472c027a98bf5069b7925c
Alejandro Laconte <lacontealejandro@gmail.com>
2017-04-11 08:26:32 -0300

    Cliente memoria con lectura de archivo config (todavia sin implementacion de getcwd hecha por Juan en Kernel

---
+++ memoria/memo.h
@@ -0,0 +1,25 @@
+/*
+ * memo.h
+ *
+ *  Created on: 9/4/2017
+ *      Author: utnso
+ */
+#ifndef MEMO_H_
+#define MEMO_H_
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include "commons/config.h"
+//#include "memo.cfg"
+
+typedef struct configMemo {
+	char* ipKernel;
+	int puertoKernel;
+} config_t;
+
+t_config* configMemo;
+config_t config;
+
+#endif /* MEMO_H_ */
