a77de58b61587b8b2147ba1c6f5031c6bc8494b8
Juan Pose <jaugusto.pose@gmail.com>
2017-04-28 20:13:06 -0300

    kernel bien

--- kernel/kernel.h
+++ kernel/kernel.h
@@ -8,27 +8,13 @@
 #ifndef KERNEL_H_
 #define KERNEL_H_
 
-#include <stdbool.h>
 #include <commons/config.h>
 #include <commons/string.h>
-#include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <sys/time.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
 #include "cliente-servidor.h"
 
-<<<<<<< HEAD:kernel/kernel.h
-struct configuracion{
-=======
-
 struct configuracion {
->>>>>>> d0a32bed51155ca9effd44b78b9f93f21e5224a3:kernel/kernel.h
+
 
 	int PUERTO_CONSOLA;
 	int PUERTO_FS;
