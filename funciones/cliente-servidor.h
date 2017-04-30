62b4429d1f0c797bc33774932b400b8f4004105e
Juan Pose <jaugusto.pose@gmail.com>
2017-04-30 14:10:02 -0300

    Update cliente-servidor.h

--- funciones/cliente-servidor.h
+++ funciones/cliente-servidor.h
@@ -1,7 +1,7 @@
 /*
  * cliente-servidor.h
  *
- *  Created on: 27/4/2017
+ *  Created on: 29/4/2017
  *      Author: utnso
  */
 
@@ -10,30 +10,11 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <arpa/inet.h>
-#include <sys/socket.h>
-#include <errno.h>
-#include <unistd.h>
 #include <sys/types.h>
+#include <sys/socket.h>
 #include <netinet/in.h>
-#include <sys/time.h>
-#include <commons/string.h>
-#include <string.h>
 
-#define MAXCLIENTS 100
 
-struct sockaddr_in direccion;
-int socketNuevasConexiones, tamanioDireccion, activado, mayorDescriptor;
-fd_set socketsParaLectura;
 
-typedef struct {
-	struct sockaddr_in addr;
-	socklen_t addrlen;
-	int socket;
-	int identidad;
-	bool atentido;
-} t_cliente;
-
-t_cliente clientes[MAXCLIENTS];
 
 #endif /* CLIENTE_SERVIDOR_H_ */
