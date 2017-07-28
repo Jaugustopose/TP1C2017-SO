729430f6ec45e1d42edadd9353bbd77f8d544b3d
Lucas Cabello <lucas_cabello@hotmail.com>
2017-07-28 17:04:35 -0300

    Deploy y makefile funciones

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,1 @@
-git clone https://github.com/sisoputnfrba/so-commons-library.git
-cd "so-commons-library"
-sudo make install
-cd ..
-git clone https://github.com/sisoputnfrba/ansisop-parser.git
-cd "ansisop-parser"
-cd parser
-sudo make install
-cd ..
-cd ..
-cd /funciones/Instalador
-sudo make install
-cd ..
-cd ..
-sudo make all
+cd kernel
