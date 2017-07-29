bf0015926e84a017b277937e621a17c0d0b2f694
Lucas Cabello <lucas_cabello@hotmail.com>
2017-07-29 00:17:50 -0300

    arreglo deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,10 @@
 git clone https://github.com/sisoputnfrba/ansisop-parser
 git clone https://github.com/sisoputnfrba/so-commons-library
+cp -ar ansisop-parser/programas-ejemplo/evaluacion-final-esther/Scripts-Prueba/ /home/utnso/scripts
+cd so-commons-library
+sudo make install
+cd ..
+cd ansisop-parser/parser
+sudo make install
+cd ..
+cd ..
